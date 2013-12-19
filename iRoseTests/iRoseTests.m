c4b99e542ef850bc77b31e1d275803cdaf30580d
luok <kevin.luo91@gmail.com>
2013-12-19 00:35:05 -0500

    Initial Commit

---
+++ iRoseTests/iRoseTests.m
@@ -0,0 +1,34 @@
+//
+//  iRoseTests.m
+//  iRoseTests
+//
+//  Created by Kai Luo on 12/19/13.
+//  Copyright (c) 2013 Kai Luo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iRoseTests : XCTestCase
+
+@end
+
+@implementation iRoseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
